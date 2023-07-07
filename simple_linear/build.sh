0ad4010c431ebdb8ce86aec849065fdca5a7db4e
DavisKoreal <daviskipchirchir19@gmail.com>
2023-07-07 10:56:13 +0300

    flow

--- simple_linear/build.sh
+++ simple_linear/build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-g++ *.cpp *.hpp -o main.exe
-./main.exe
-cd .. && ./*changes && cd *linear
+build_and_push() 
+{
+    g++ *.cpp *.hpp -o main.exe && ./main.exe
+    cd .. && ./*changes && cd *linear
+}
+
+build_and_push "flow"
